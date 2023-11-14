a719b7d1f8f48bcb6d284c7c2c75d9bd62545004
rexmortus <alecs@eml.cc>
2023-11-15 09:57:49 +1100

    Simple implementation

---
+++ scripts/lib/forms/SimpleTokenMovementForm.js
@@ -0,0 +1,134 @@
+export class SimpleTokenMovementForm extends FormApplication {
+
+    // The constructor needs both the timekeeper and journalkeeper
+    constructor() {
+        super();
+    }
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ["form"],
+            popOut: true,
+            template: "modules/simple-token-movement/scripts/lib/forms/SimpleTokenMovementForm.html",
+            id: "simple-token-movement",
+            title: "Simple Token Movement",
+            height: 400,
+            width: 400,
+            minimizable: false,
+            classes: ["simple-token-movement"]
+        });
+    }
+
+  getToken() {
+    let token = game.user.character.getActiveTokens()[0]
+    token.control({ releaseOthers: true })
+    token.border.visible = false
+    return token
+  }
+
+  move(x, y) {
+    let t = this.getToken()
+    let newX = t.x + t.w * x
+    let newY = t.y + t.h * y
+    const newPoint = canvas.grid.getSnappedPosition(newX, newY)
+    if (!t.checkCollision(newPoint)) {
+      t.document.update(newPoint)
+      canvas.animatePan({
+        duration: 250,
+        x: newPoint.x + t.w / 2,
+        y: newPoint.y + t.h / 2,
+        scale: canvas.scene._viewPosition.scale,
+      })
+    }
+  }
+
+  selectToken() {
+    let t = this.getToken()
+    canvas.animatePan({
+      duration: 150,
+      x: t.x + t.w / 2,
+      y: t.y + t.h / 2,
+      scale: canvas.scene._viewPosition.scale,
+    })
+  }
+
+  zoomIn() {
+    const view = canvas.scene._viewPosition
+    canvas.animatePan({
+      duration: 200,
+      x: view.x,
+      y: view.y,
+      scale: view.scale * 1.25,
+    })
+  }
+
+  zoomOut() {
+    const view = canvas.scene._viewPosition
+    canvas.animatePan({
+      duration: 200,
+      x: view.x,
+      y: view.y,
+      scale: view.scale * 0.80,
+    })
+  }
+
+  moveTopLeft() {
+    this.move(-1, -1)
+  }
+
+  moveLeft() {
+    this.move(-1, 0)
+  }
+
+  moveBottomLeft() {
+    this.move(-1, 1)
+  }
+
+  moveTop() {
+    this.move(0, -1)
+  }
+
+  moveBottom() {
+    this.move(0, 1)
+  }
+
+  moveTopRight() {
+    this.move(1, -1)
+  }
+
+  moveRight() {
+    this.move(1, 0)
+  }
+
+  moveBottomRight() {
+    this.move(1, 1)
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html)
+    $('.mtmc-select', html).click($.proxy(this.selectToken, this))
+    $('.mtmc-zoomin', html).click($.proxy(this.zoomIn, this))
+    $('.mtmc-zoomout', html).click($.proxy(this.zoomOut, this))
+    $('.mtmc-topleft', html).click($.proxy(this.moveTopLeft, this))
+    $('.mtmc-left', html).click($.proxy(this.moveLeft, this))
+    $('.mtmc-bottomleft', html).click($.proxy(this.moveBottomLeft, this))
+    $('.mtmc-top', html).click($.proxy(this.moveTop, this))
+    $('.mtmc-bottom', html).click($.proxy(this.moveBottom, this))
+    $('.mtmc-topright', html).click($.proxy(this.moveTopRight, this))
+    $('.mtmc-right', html).click($.proxy(this.moveRight, this))
+    $('.mtmc-bottomright', html).click($.proxy(this.moveBottomRight, this))
+  }
+
+
+
+    getData() {
+
+        // Create an object that is passed to the view template
+        return {
+
+        };
+    }
+
+    async _updateObject(event, formData) {
+    }
+}
--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,18 @@
+import { SimpleTokenMovementForm } from './lib/forms/SimpleTokenMovementForm.js'
+
 Hooks.once('init', async function() {
 
 });
 
 Hooks.once('ready', async function() {
+
+    const mainForm = new SimpleTokenMovementForm();
+
+    mainForm.render(true);
+
+    // Show form
+    Hooks.on('simple-token-movement.openForm', function(tick) {
+        mainForm.render(true);
+    });
 
 });
