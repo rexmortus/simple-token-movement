de9f77e182e5b643da4fdcded66ec95d72b7b2b8
rexmortus <alecs@eml.cc>
2023-12-29 11:42:08 +1100

    Fix touch scrolling issue

--- scripts/lib/forms/SimpleTokenMovementForm.js
+++ scripts/lib/forms/SimpleTokenMovementForm.js
@@ -4,6 +4,10 @@ export class SimpleTokenMovementForm extends FormApplication {
       super();
       this.socket = socket;
       this.actionList = game.modules.get('character-actions-list-5e').api.getActorActionsData(game.user.character);
+      this.startElement;
+      this.touchStartX;
+      this.touchStartY; 
+      this.scrolled;
     }
 
     static get defaultOptions() {
@@ -66,6 +70,37 @@ export class SimpleTokenMovementForm extends FormApplication {
     action.use();
   }
 
+  onTouchStart(event) {
+    // Store the element where touchstart occurred
+    this.startElement = event.currentTarget;
+    this.touchStartX = event.touches[0].clientX;
+    this.touchStartY = event.touches[0].clientY;
+    this.scrolled = false;
+    console.log(`${this.touchStartX}, ${this.touchStartY}`)
+  }
+
+  onTouchMove(event) {
+    // Determine if the touch has moved significantly
+    if (Math.abs(event.touches[0].clientX - this.touchStartX) > 10 || 
+        Math.abs(event.touches[0].clientY - this.touchStartY) > 10) {
+        this.scrolled = true;
+    }
+  } 
+
+  onTouchEnd(event) {
+    let eventType = $(this.startElement).data('actionType');
+    let eventIndex = $(this.startElement).data('actionIndex');
+    let action = [...this.actionList[eventType]][eventIndex]
+
+    // Check if the touchend event is on the same element
+    if (event.currentTarget === this.startElement && !this.scrolled) {
+        action.use();
+    }
+      
+    // Reset the startElement for the next touch
+    this.startElement = null;
+  }
+
   activateListeners(html) {
     super.activateListeners(html)
     $('.mtmc-select', html).bind('touchstart click', $.proxy(this.selectToken, this))
@@ -79,8 +114,9 @@ export class SimpleTokenMovementForm extends FormApplication {
     $('.mtmc-topright', html).bind('touchstart click', $.proxy(this.moveTopRight, this))
     $('.mtmc-right', html).bind('touchstart click', $.proxy(this.moveRight, this))
     $('.mtmc-bottomright', html).bind('touchstart click', $.proxy(this.moveBottomRight, this))
-
-    $('[data-action-type][data-action-index].token-controller-action', html).bind('touchstart click', $.proxy(this.doAction, this));
+    $('[data-action-type][data-action-index].token-controller-action', html).bind('touchstart', $.proxy(this.onTouchStart, this))
+    $('[data-action-type][data-action-index].token-controller-action', html).bind('touchend', $.proxy(this.onTouchEnd, this))
+    $('[data-action-type][data-action-index].token-controller-action', html).bind('touchmove', $.proxy(this.onTouchMove, this))
   }
 
   getData() {
